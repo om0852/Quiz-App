2ec2fd201df86284f367fea421e33fa98e84e56e
omsalunke0852 <103877025+om0852@users.noreply.github.com>
2024-02-26 09:18:04 +0530

    quiz page phase1

--- app/src/main/java/com/example/microproject/MainActivity.java
+++ app/src/main/java/com/example/microproject/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.microproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button easyBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        easyBtn = findViewById(R.id.easyBtn);
+        easyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent t1 = new Intent(MainActivity.this, quiz_page.class);
+                startActivity(t1);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/microproject/quiz_page.java
@@ -0,0 +1,68 @@
+package com.example.microproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class quiz_page extends AppCompatActivity {
+    Button option1,option2,option3,option4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_page);
+         option1 =findViewById(R.id.option1);
+         option2 =findViewById(R.id.option2);
+         option3 =findViewById(R.id.option3);
+         option4 =findViewById(R.id.option4);
+         option1.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 option_fun1();
+             }
+         });option2.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 option_fun2();
+             }
+         });option3.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 option_fun3();
+             }
+         });option4.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 option_fun4();
+             }
+         });
+
+    }
+
+    public void option_fun1() {
+        Toast.makeText(this, "option1 click", Toast.LENGTH_SHORT).show();
+        option1.setBackgroundColor(Color.BLUE);
+        option2.setBackgroundColor(Color.RED);
+        option3.setBackgroundColor(Color.RED);
+        option4.setBackgroundColor(Color.RED);
+    }
+    public void option_fun2() {
+        option1.setBackgroundColor(Color.RED);
+        option2.setBackgroundColor(Color.BLUE);
+        option3.setBackgroundColor(Color.RED);
+        option4.setBackgroundColor(Color.RED);    }
+    public void option_fun3() {
+        option1.setBackgroundColor(Color.RED);
+        option2.setBackgroundColor(Color.RED);
+        option3.setBackgroundColor(Color.BLUE);
+        option4.setBackgroundColor(Color.RED);
+    }
+    public void option_fun4() {
+        option1.setBackgroundColor(Color.RED);
+        option2.setBackgroundColor(Color.RED);
+        option3.setBackgroundColor(Color.RED);
+        option4.setBackgroundColor(Color.BLUE);    }
+}
